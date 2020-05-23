cee6031d96e9ba86de6645a204f9447d72666368
Supriya Shelar <supriya.shelar07@gmail.com>
2020-05-23 18:59:36 +0530

    Multithreading with Spring Boot

---
+++ multithreading-app/src/main/java/com/stech/demo/multithreadingapp/MultithreadingAppApplication.java
@@ -0,0 +1,13 @@
+package com.stech.demo.multithreadingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingAppApplication.class, args);
+	}
+
+}
---
+++ multithreading-app/src/test/java/com/stech/demo/multithreadingapp/MultithreadingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stech.demo.multithreadingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
